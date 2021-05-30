72fdb378ecfa42952ec12292ba05541b14eec008
apsmj23 <apurvalion@gmail.com>
2021-05-30 09:08:46 +0530

    asad

--- NBA/settings.py
+++ NBA/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['127.0.0.1','nbaapp14.herokuapp.com']
 INSTALLED_APPS = [
     'users',
     'team',
-    'multiforloop',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
